d363adaa949ce180c657bd1bdcf60dbdc89e7025
Gustavo Mesquita <g.caldeira@edu.pucrs.br>
2023-10-02 22:38:03 -0300

    Tentei ajeitar as classes, porem estou com problemas na implementacao dos metodos na app

---
+++ t1-gcs/src/Medico.java
@@ -0,0 +1,26 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class Medico extends Usuario {
+    private List<AutorizacaoExame> autorizacoes;
+
+    public Medico(int identificador, String nome) {
+        super(identificador, nome, 3);
+        autorizacoes = new ArrayList<>();
+    }
+
+    public void incluirNovaAutorizacaoExame(Paciente paciente, Exame exame) {
+        AutorizacaoExame autorizacao = new AutorizacaoExame(AutorizacaoExame.getCodigoSequencial(), this, paciente, exame);
+        autorizacoes.add(autorizacao);
+        System.out.println("Nova autorizacao de exame adicionada com sucesso!");
+    }
+
+    public List<AutorizacaoExame> listarAutorizacoes(String filtro) {
+        return autorizacoes.stream()
+               .filter(autorizacao -> autorizacao.getPaciente().getNome().equalsIgnoreCase(filtro) ||
+                       autorizacao.getTipoExame().getNomeExame().equalsIgnoreCase(filtro))
+               .collect(Collectors.toList());
+    }
+
+}
